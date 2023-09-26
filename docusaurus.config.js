bf28597a3ae80d600e17fe1a9461d9e0a77f717e
Kyle Martin <kyle.martin@middleware.co.nz>
2023-09-27 08:59:27 +1300

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/apistds-docusaurus',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kyle-mwnz', // Usually your GitHub org/user name.
+  projectName: 'apistds-docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'ignore', // Set this to true later
   onBrokenMarkdownLinks: 'warn',
